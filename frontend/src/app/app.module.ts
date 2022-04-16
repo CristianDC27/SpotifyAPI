6c5a4bdad4e48f999af89f73348f828067a84f66
Cristian David Casado <cristiancasado27@gmail.com>
2022-04-16 12:31:20 +0200

    Adapted repo from tasty

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,29 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatIconModule } from "@angular/material/icon";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RecipeCardComponent } from './recipe-card/recipe-card.component';
-import { MenuTabsComponent } from './menu-tabs/menu-tabs.component';
 import { MatCardModule } from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SearchByCategoriesComponent } from './search-by-categories/search-by-categories.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
-import { RecipeCardDetailComponent } from './recipe-card-detail/recipe-card-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeCardComponent,
-    MenuTabsComponent,
-    RecipeListComponent,
-    SearchBarComponent,
-    SearchByCategoriesComponent,
-    RecipeCardDetailComponent
   ],
   imports: [
     BrowserModule,
