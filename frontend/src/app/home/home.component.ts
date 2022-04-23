367de34679fe9012ec1a88a1927cc4890baec67d
Cristian David Casado <cristiancasado27@gmail.com>
2022-04-23 16:41:44 +0200

    Horizontal Scroller created

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -10,6 +10,13 @@ import { SpotifyService } from '../spotify.service';
 export class HomeComponent implements OnInit {
   albums?: Album[];
 
+  opts = {
+    slidesPerView: 2.4,
+    slidesOffsetBefore: 20,
+    spaceBetween: 20,
+    freeMode: true
+  };
+
   constructor(private spotifyService: SpotifyService) { }
 
   ngOnInit(): void {
