3d1b95ff802194b724f47410cfb5447922ff3410
Cristian David Casado <cristiancasado27@gmail.com>
2022-05-01 21:46:32 +0200

    Test passed

--- frontend/src/app/track/track.component.spec.ts
+++ frontend/src/app/track/track.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TrackComponent } from './track.component';
@@ -8,7 +9,8 @@ describe('TrackComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TrackComponent ]
+      declarations: [ TrackComponent ],
+      imports: [HttpClientModule]
     })
     .compileComponents();
   });
