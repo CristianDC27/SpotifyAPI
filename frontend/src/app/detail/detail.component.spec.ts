736b687382d5120a7f182cd6903e22f0384dea55
Cristian David Casado <cristiancasado27@gmail.com>
2022-05-01 21:46:32 +0200

    Test passed

--- frontend/src/app/detail/detail.component.spec.ts
+++ frontend/src/app/detail/detail.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DetailComponent } from './detail.component';
@@ -8,7 +9,8 @@ describe('DetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DetailComponent ]
+      declarations: [ DetailComponent ],
+      imports: [HttpClientModule]
     })
     .compileComponents();
   });
