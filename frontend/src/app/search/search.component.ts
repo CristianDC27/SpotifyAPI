ff2e1884223256905ca0f88e218b1530ee680e13
Cristian David Casado <cristiancasado27@gmail.com>
2022-05-23 11:33:10 +0200

    Search view

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from 'src/album';
+import { Artist } from 'src/artist';
+import { Show } from 'src/show';
+import { Track } from 'src/track';
+import { SpotifyService } from '../spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  albums?: Album[];
+  artists?: Artist[];
+  shows?: Show[];
+  tracks?: Track[];
 
-  constructor() { }
+  constructor(private spotifyService: SpotifyService) { }
 
   ngOnInit(): void {
   }
 
+  onSearch(value: String): void {
+    this.spotifyService.getSearch(10,value).subscribe( (json:any) => {
+      this.albums = json.albums.items;
+      this.artists = json.artists.items;
+      this.shows = json.shows.items;
+      this.tracks = json.tracks.items;
+    });
+  }
 }
